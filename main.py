75e4694e4094ecd71c72d53bf74846384b930606
sehitkaradag <62108826+sehitkaradag@users.noreply.github.com>
2023-11-11 18:04:45 +0100

    text

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from PIL import Image
 
+st.set_page_config(layout="wide", page_title = "Palestine starterkit")
+image = Image.open(Flag_of_Palestine.svg.png)
 st.write("This will be for Palestine!")
