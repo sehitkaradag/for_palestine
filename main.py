7068abb6a48298d2e0b7d629b5fb90dc45051514
sehitkaradag <62108826+sehitkaradag@users.noreply.github.com>
2023-11-11 18:40:53 +0100

    terug

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ st.set_page_config(page_icon = "Flag_of_Palestine_Flat_Round.png" ,page_title = 
 st.title("Wat jij kan doen voor Palestina")
 st.divider()
 
+st.subheader("Zelf actie voeren", divider='red')
 
-with element:
-    st.subheader("Zelf actie voeren", divider='red')
-    st.write("Hoe meer demonstraties & acties, hoe beter! Het is super belangrijk dat we op zoveel mogelijk plekken van ons laten horen. Een actie of demonstratie kan zo groot of klein zijn als je zelf wilt - je kunt zonder voorbereiding in je eentje op een drukke plek gaan staan met een pakkend bord of leuzen op de stoep krijten. Maar je kan ook een demonstratie met duizenden mensen organiseren, en van alles daar tussenin.")
-    st.link_button(":red[Demonstratie starterkit]","https://drive.google.com/file/d/10XuVK22QailEljV--nZ7Ca-1Xc1tXvSx/view?usp=sharing")
-
-st.sidebar.element
+st.write("Hoe meer demonstraties & acties, hoe beter! Het is super belangrijk dat we op zoveel mogelijk plekken van ons laten horen. Een actie of demonstratie kan zo groot of klein zijn als je zelf wilt - je kunt zonder voorbereiding in je eentje op een drukke plek gaan staan met een pakkend bord of leuzen op de stoep krijten. Maar je kan ook een demonstratie met duizenden mensen organiseren, en van alles daar tussenin.")
+st.link_button(":red[Demonstratie starterkit]","https://drive.google.com/file/d/10XuVK22QailEljV--nZ7Ca-1Xc1tXvSx/view?usp=sharing")
