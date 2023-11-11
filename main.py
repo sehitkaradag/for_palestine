ea70c7c6648d70ce6c7d80e6753824e45bfdcc54
sehitkaradag <62108826+sehitkaradag@users.noreply.github.com>
2023-11-11 18:18:41 +0100

    first text

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_icon = "Flag_of_Palestine_Flat_Round.png" ,page_title = "Palestine starterkit")
+
 st.title("Wat jij kan doen voor Palestina")
+st.divider()
+st.write("Er worden super veel acties opgezet op het moment! Maar wat gebeurt er allemaal, waar en wanneer? En wat kan jij doen? Deze lijst is helaas niet volledig maar geeft een beeld van wat je vanuit Nederland kunt doen voor een vrij Palestina. Bijvoorbeeld naar demonstraties gaan, zelf een demonstratie of actie organiseren, doneren aan acties, boycotten, spullen kopen, posters ophangen, politici mailen of petities tekenen. En ook heel belangrijk: spreek je uit!")
