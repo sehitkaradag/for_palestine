4d841e1c1d5b3ab30a3f2ef11b881bcb8b9d8bc8
sehitkaradag <62108826+sehitkaradag@users.noreply.github.com>
2023-11-11 18:28:44 +0100

    button test

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ st.write("Er worden super veel acties opgezet op het moment! Maar wat gebeurt er
 st.subheader("Zelf actie voeren", divider='red')
 
 st.write("Hoe meer demonstraties & acties, hoe beter! Het is super belangrijk dat we op zoveel mogelijk plekken van ons laten horen. Een actie of demonstratie kan zo groot of klein zijn als je zelf wilt - je kunt zonder voorbereiding in je eentje op een drukke plek gaan staan met een pakkend bord of leuzen op de stoep krijten. Maar je kan ook een demonstratie met duizenden mensen organiseren, en van alles daar tussenin.")
+st.link_button("Demonstratie starterkit","https://drive.google.com/file/d/10XuVK22QailEljV--nZ7Ca-1Xc1tXvSx/view?usp=sharing")
