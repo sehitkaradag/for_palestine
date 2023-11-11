ee0c036e7c18f96d5bf64ca35ae126ad8baec4eb
sehitkaradag <62108826+sehitkaradag@users.noreply.github.com>
2023-11-11 18:20:28 +0100

    subheader

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ st.set_page_config(page_icon = "Flag_of_Palestine_Flat_Round.png" ,page_title = 
 st.title("Wat jij kan doen voor Palestina")
 st.divider()
 st.write("Er worden super veel acties opgezet op het moment! Maar wat gebeurt er allemaal, waar en wanneer? En wat kan jij doen? Deze lijst is helaas niet volledig maar geeft een beeld van wat je vanuit Nederland kunt doen voor een vrij Palestina. Bijvoorbeeld naar demonstraties gaan, zelf een demonstratie of actie organiseren, doneren aan acties, boycotten, spullen kopen, posters ophangen, politici mailen of petities tekenen. En ook heel belangrijk: spreek je uit!")
+
+st.subheader("Zelf actie voeren", divider='red')
