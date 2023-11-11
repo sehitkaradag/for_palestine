b4348df8c3d11497b8c73f74e76550ff65dc2e66
sehitkaradag <62108826+sehitkaradag@users.noreply.github.com>
2023-11-11 18:13:36 +0100

    center

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.set_page_config(layout="wide",page_icon = "Flag_of_Palestine_Flat_Round.png" ,page_title = "Palestine starterkit")
+st.set_page_config(page_icon = "Flag_of_Palestine_Flat_Round.png" ,page_title = "Palestine starterkit")
 st.write("This will be for Palestine!")
