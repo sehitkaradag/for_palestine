e5da0e501e2ffe6b5c82802344eb1b48d10242ff
sehitkaradag <62108826+sehitkaradag@users.noreply.github.com>
2023-11-15 01:13:19 +0100

    D

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ with st.sidebar:
 ######################################
 # Page title & Dataframe display
 ######################################
-st.warning("LANDELIJKE DEMONSTRATIE 19 NOVEMBER ROTTERDAM 14:00")
+st.error("LANDELIJKE DEMONSTRATIE 19 NOVEMBER ROTTERDAM 14:00 BINNENROTTE")
 st.subheader("Palestina protesten en informatie")
 
 data_df = pd.DataFrame(
